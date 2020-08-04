329c284c4169a5d6dd133b2a5238384da4b634ed
harika penukonda <penukondaharika@gmail.com>
2020-08-04 10:54:25 -0500

    inital commit

---
+++ src/com/udemy/aopdemo/AfterThrowingDemoApp.java
@@ -0,0 +1,40 @@
+package com.udemy.aopdemo;
+
+import java.util.List;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.udemy.aopdemo.dao.AccountDAO;
+import com.udemy.aopdemo.dao.MembershipDAO;
+
+public class AfterThrowingDemoApp {
+
+	public static void main(String[] args) {
+		
+		// read the spring config java class
+		AnnotationConfigApplicationContext context = 
+				new AnnotationConfigApplicationContext(DemoConfig.class);
+		
+		// get the bean from spring container
+		AccountDAO theAccountDAO = 
+				context.getBean("accountDAO", AccountDAO.class);
+		
+		// call method to find the accounts
+		List<Account> theAccounts = null;
+		try {
+			theAccounts = theAccountDAO.findAccounts();
+		} catch (Exception e) {
+			System.out.println("\n\nMain Program... caught execpetion: " +e);
+		} 
+		
+		// display the accountsß
+		System.out.println("\n\nMain Program: AfterThrowingDemoApp");
+		System.out.println("-----");
+		System.out.println(theAccounts);
+		System.out.println("\n");
+		
+		// close the context
+		context.close();
+	}
+
+}
